ac547a89c5ee6f27d2d470409356ccbb6755d273
jjiajun <teo.jia.jun.29@gmail.com>
2022-02-03 18:41:24 +0800

    all videos aligned horizontally

--- src/components/allVideoFeed.jsx
+++ src/components/allVideoFeed.jsx
@@ -11,24 +11,28 @@ const AllVideoFeed = ({
 }) => {
   return (
     <>
-      <div id="video-grid">
-        <div>
-          <video muted ref={userVideo} autoPlay playsInline></video>
-          {/* <p>LearnerId: {learnerId.current}</p> */}
-          {/* <p>{learnerName.current} </p> */}
+      <div className="video-container d-flex align-items-center">
+        <div className="row video-row">
+          <div className="col video-col">
+            <video muted ref={userVideo} autoPlay playsInline></video>
+            {/* <p>LearnerId: {learnerId.current}</p> */}
+            {/* <p>{learnerName.current} </p> */}
+          </div>
+          {peers.map((peer, index) => {
+            return (
+              <VideoFeed
+                key={peer.peerId}
+                peerId={peer.peerId}
+                peer={peer.peer}
+                learnerId={peer.learnerId}
+                learnerName={peer.learnerName}
+                socket={socket}
+                noScreens={peers.length + 1}
+                index={index}
+              />
+            );
+          })}
         </div>
-        {peers.map((peer) => {
-          return (
-            <VideoFeed
-              key={peer.peerId}
-              peerId={peer.peerId}
-              peer={peer.peer}
-              learnerId={peer.learnerId}
-              learnerName={peer.learnerName}
-              socket={socket}
-            />
-          );
-        })}
       </div>
     </>
   );
